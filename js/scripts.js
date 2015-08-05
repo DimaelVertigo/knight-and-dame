62f3761c745226db060c7374503a73f469073f3f
DimaelVertigo <dimael.vertigo@gmail.com>
2015-08-05 17:27:08 +0300

    Add faq page

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,20 @@ $(document).ready(function() {
 		navText: false
 	});
 
+	var allPanels = $('.answer');
+	allPanels.hide();
+	$('.question').on('click', function() {
+		var _this = $(this),
+			answer  = _this.next('.answer'),
+			speed = 250;
+		_this.parent().siblings().children('.question').removeClass('question-active');
+		_this.parent().siblings().children('.answer').slideUp(speed);
+		if (answer.is(':visible')) {
+			_this.removeClass('question-active');
+			answer.slideUp(speed);
+		} else {
+			_this.addClass('question-active');
+			answer.slideDown(speed);
+		}
+	});
 });
